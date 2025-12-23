6b0bb7d1c5f34e7d4e9148db04c543026c97c0bf
MadridMAC <anymordonutzarty@gmail.com>
2025-12-23 13:45:18 +0800

    Added custom handler for readiness endpoint (/healthz)

--- main.go
+++ main.go
@@ -4,6 +4,14 @@ import "net/http"
 
 func main() {
 	new_mux := http.NewServeMux()
+	new_mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+
+	new_mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	})
+
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: new_mux,
